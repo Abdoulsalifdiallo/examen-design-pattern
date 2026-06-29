678f5de4b782aab0a68e6796ac1eb3279038cdaf
Abdoulsalifdiallo <abdoulsalifdiallo22@gmail.com>
2026-06-29 09:29:11 +0000

    chore: bootstrap des squelettes Maven badwallet-api et payment-service

---
+++ badwallet-api/src/main/java/sn/examen/designpattern/badwalletapi/BadwalletApiApplication.java
@@ -0,0 +1,14 @@
+package sn.examen.designpattern.badwalletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class BadwalletApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadwalletApiApplication.class, args);
+    }
+}
